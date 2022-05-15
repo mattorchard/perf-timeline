b5937fa894b2b14eb619300913a06ad5cc4f3512
Matt Orchard <morch028@uottawa.ca>
2022-05-15 18:06:26 -0400

    Switch to adapter static

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,9 +6,11 @@ const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
+	// { sourceMap: false }
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: { default: true }
 	}
 };
 
